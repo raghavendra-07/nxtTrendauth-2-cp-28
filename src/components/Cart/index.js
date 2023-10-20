bcfa89732872746aaf33afec59b1380b2ff4fd4b
raghavendra <raghavendrachary010@gmail.com>
2023-10-20 09:46:21 +0000

    commit message

---
+++ src/components/Cart/index.js
@@ -0,0 +1,16 @@
+// Write your JS code here
+import Header from '../Header'
+
+const Cart = () => (
+  <div>
+    <Header />
+    <div>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+        alt="cart"
+      />
+    </div>
+  </div>
+)
+
+export default Cart
---
+++ src/components/Products/index.js
@@ -0,0 +1,16 @@
+// Write your JS code here
+import Header from '../Header'
+
+const Products = () => (
+  <div>
+    <Header />
+    <div>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+        alt="products"
+      />
+    </div>
+  </div>
+)
+
+export default Products
